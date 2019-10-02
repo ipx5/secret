085333811e118a7a3d581b25c046d451c0d98e8b
System Administrator <polyakovx5@gmail.com>
2019-10-02 10:54:58 +0300

    sing_up_page

--- secret_js/src/app/app.routes.js
+++ secret_js/src/app/app.routes.js
@@ -4,6 +4,7 @@ import {notFound} from "./shared/not-found.component";
 import {directivePageComponent} from "./pages/directive-page.component";
 import {pipePageComponent} from "./pages/pipes-page.component";
 import { siPageComponent } from "./pages/si-page.component";
+import { suPageComponent } from "./pages/su-page.component";
 
 export const appRoutes = [
     { path: '', component: homePageComponent },
@@ -12,4 +13,5 @@ export const appRoutes = [
     {path: '**', component: notFound},
     { path: 'pipe', component: pipePageComponent },
     { path: 'si', component: siPageComponent },
+    { path: 'su', component: suPageComponent },
 ];
--- secret_js/src/app/pages/si-page.component.js
+++ secret_js/src/app/pages/si-page.component.js
@@ -31,14 +31,12 @@ export const siPageComponent = new SiPageComponent({
     <main>
     <center>
       <div class="container">
-        <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
-
+        <div class="row" style="display: inline-block; padding: 32px 48px 0px 48px;">
           <form class="col s12" method="post">
             <div class='row'>
               <div class='col s12'>
               </div>
             </div>
-
             <div class='row'>
               <div class='input-field col s12'>
                 <input class='validate' type='email' name='email' id='email' />
@@ -63,7 +61,7 @@ export const siPageComponent = new SiPageComponent({
               </div>
             </center>
           </form>
-      <a class='black-text' href="#login">{{ createuser }}</a>
+      <a class='black-text' href="#su">{{ createuser }}</a>
     </center>
   </main>
         `,
